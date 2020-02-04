8a130cd8187633199b57a151bf78de1c78a4245a
aicquewk <vuttithatkrongyot@gmail.com>
2020-02-04 15:12:11 +0700

    add config detail

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC_K7EYmdK1FjLOD0y_772RuiEc_Dicv5U",
+    authDomain: "hostel-management-existing.firebaseapp.com",
+    databaseURL: "https://hostel-management-existing.firebaseio.com",
+    projectId: "hostel-management-existing",
+    storageBucket: "hostel-management-existing.appspot.com",
+    messagingSenderId: "933709017396",
+    appId: "1:933709017396:web:bed65bf50006b78f51b561",
+    measurementId: "G-VTNNBS0J49"
+  };
+
+  export default firebaseConfig
